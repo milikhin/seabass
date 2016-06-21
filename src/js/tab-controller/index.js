6ac4dd9b862434a912e9a92e1ab72bebd3cd74d0
mikhael <mikhael@milikhin.name>
2016-06-21 12:16:00 +0700

    add more languages

--- src/js/tab-controller/index.js
+++ src/js/tab-controller/index.js
@@ -8,22 +8,27 @@ define([
 	'ace/worker/worker',
 
 	'ace/mode/javascript',
+	'ace/mode/typescript',
 	'ace/mode/json',
 	'ace/mode/css',
-	'ace/mode/html',
-	'ace/mode/php',
-	'ace/mode/ejs',
-	'ace/mode/jsx',
 	'ace/mode/less',
 	'ace/mode/sass',
 	'ace/mode/scss',
 	'ace/mode/stylus',
+
+	'ace/mode/html',
+	'ace/mode/svg',
+	'ace/mode/php',
+	'ace/mode/ejs',
+	'ace/mode/jsx',
+	'ace/mode/ruby',
 
 	'ace/mode/c_cpp',
 	'ace/mode/golang',
 	'ace/mode/python',
 	'ace/mode/java',
 
+	'ace/mode/sh',
 	'ace/mode/plain_text'
 
 
