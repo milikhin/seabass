a771f69f9a0e4e13a645a7cb5c048db6b683b0fe
mikhael <mikhael@milikhin.name>
2016-12-28 17:20:32 +0700

    use single package.json for npm and nwjs

--- dev-tools/tasks/copy-only.js
+++ dev-tools/tasks/copy-only.js
@@ -7,9 +7,9 @@ module.exports = function() {
         // var imgs = gulp.src(['images/**/*']).pipe(gulp.dest('./dist/images'));
         // var fonts = gulp.src(['fonts/**/*']).pipe(gulp.dest('./dist/fonts'));
         var etc = gulp.src(['src/public/**/*']).pipe(gulp.dest('./www/'));
-        // var js = gulp.src(['tmp/js/app/index*']).pipe(gulp.dest('./dist/js/'));
+        var packageJson = gulp.src(['package.json']).pipe(gulp.dest('./www/'));
         // var ghFiles = gulp.src(['CNAME']).pipe(gulp.dest('./dist/'));
 
-        return merge( /*imgs, fonts, ghFiles,*/ etc);
+        return merge( /*imgs, fonts, ghFiles,*/ packageJson, etc);
     };
 };
