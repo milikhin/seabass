98b012c1e4ea59db596a7ef24923b45f72657fe5
Brian Douglass <bhdouglass@gmail.com>
2016-12-31 10:45:56 -0500

    Fixed setup-unconfined script

--- setup-unconfined.sh
+++ setup-unconfined.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i 's/var policy = { policy_groups: ['networking', 'audio'], policy_version: 1};/var policy = { policy_groups: ['networking', 'audio'], policy_version: 1, "write_path": ["@{HOME}/"] };/g platforms/ubuntu/cordova/lib/manifest.js'
+sed -i -e 's#policy_version: 1#policy_version: 1, write_path: ["@{HOME}/"]#g' platforms/ubuntu/cordova/lib/manifest.js
