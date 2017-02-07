f453ae956a870be5c5c06a370d65b92e7fe26b3a
mikhael <mikhael@milikhin.name>
2017-02-07 13:16:29 +0700

    add On/RemoveEventListener methods to AppEvent singleton

--- src/js/app-event.js
+++ src/js/app-event.js
@@ -3,12 +3,34 @@ define([], function() {
 
     }
 
-    AppEventController.prototype.dispatch = function(evtDetail) {
+    AppEventController.prototype.dispatch = function(evtName, evtDetail) {
+        if (!evtName) {
+            throw new Error("AppEvent name is missing");
+        }
+
+        evtDetail = evtDetail || {};
+        evtDetail.name = evtName;
         document.body.dispatchEvent(new CustomEvent('app-event', {
             bubbles: true,
             cancelable: true,
             detail: evtDetail || {}
         }));
+    };
+
+    AppEventController.prototype.on = function(evtName, callback) {
+        let callbackFunction = function(evt) {
+            if (!evt.detail.type) {
+                throw new Error("AppEvent detail is missing", evt);
+            }
+            callback(evt.detail.type, evt);
+        };
+
+        document.body.addEventListener("app-event", callbackFunction);
+        return callbackFunction;
+    };
+  
+  	AppEventController.prototype.removeEventListener = function(callbackOnFunction) {
+        document.body.removeEventListener("app-event", callbackOnFunction);        
     };
 
     return new AppEventController();
