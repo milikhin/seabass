ea135ae3d499a0a6fbaf08b58acc7bba9ccc9b64
mikhael <mikhael@milikhin.name>
2017-06-03 18:39:53 +0700

    remove OSK-mode button

--- src/js/ui/buttons.js
+++ src/js/ui/buttons.js
@@ -1,47 +1,50 @@
 define([], function() {
     return function() {
         return [{
-            action: "editor-save",
-            iconClass: "save",
-            text: "Save<br/><code>Ctrl + S</code>",
-            addons: "disabled",
-            doNotShow: true,
-            className: 'header__tab__button-pane__button-save',
-            priority: 0
-        }, {
-            action: "tree__toggle",
-            iconClass: "tree",
-            text: "Toggle file tree<br/><code>Ctrl + T</code>",
-            className: 'header__tab__button-pane__button-ftree',
-            priority: 0
-        }, {
-            action: "window-osk__toggle",
-            iconClass: "toggle-osk",
-            text: "Toggle OSK mode",
-            className: 'header__tab__button-pane__button-osk',
-            priority: 10
-        }, {
-            action: "editor-beautify",
-            iconClass: "beautify",
-            text: "Beautify<br/><code>Ctrl + Alt + B</code>",
-            addons: "disabled",
-            className: 'header__tab__button-pane__button-beautify',
-            priority: 20
-        }, {
-            action: "editor-redo",
-            iconClass: "redo",
-            text: "Redo<br/><code>Ctrl + Y</code>",
-            addons: "disabled",
-            // doNotShow: true,
-            className: 'header__tab__button-pane__button-redo',
-            priority: 30
-        }, {
-            action: "editor-undo",
-            iconClass: "undo",
-            text: "Undo<br/><code>Ctrl + Z</code>",
-            addons: "disabled",
-            className: 'header__tab__button-pane__button-undo',
-            priority: 40
-        }];
+                action: "editor-save",
+                iconClass: "save",
+                text: "Save<br/><code>Ctrl + S</code>",
+                addons: "disabled",
+                doNotShow: true,
+                className: 'header__tab__button-pane__button-save',
+                priority: 0
+            }, {
+                action: "tree__toggle",
+                iconClass: "tree",
+                text: "Toggle file tree<br/><code>Ctrl + T</code>",
+                className: 'header__tab__button-pane__button-ftree',
+                priority: 0
+            },
+            //         {
+            //     action: "window-osk__toggle",
+            //     iconClass: "toggle-osk",
+            //     text: "Toggle OSK mode",
+            //     className: 'header__tab__button-pane__button-osk',
+            //     priority: 10
+            // }, 
+            {
+                action: "editor-beautify",
+                iconClass: "beautify",
+                text: "Beautify<br/><code>Ctrl + Alt + B</code>",
+                addons: "disabled",
+                className: 'header__tab__button-pane__button-beautify',
+                priority: 20
+            }, {
+                action: "editor-redo",
+                iconClass: "redo",
+                text: "Redo<br/><code>Ctrl + Y</code>",
+                addons: "disabled",
+                // doNotShow: true,
+                className: 'header__tab__button-pane__button-redo',
+                priority: 30
+            }, {
+                action: "editor-undo",
+                iconClass: "undo",
+                text: "Undo<br/><code>Ctrl + Z</code>",
+                addons: "disabled",
+                className: 'header__tab__button-pane__button-undo',
+                priority: 40
+            }
+        ];
     };
 });
