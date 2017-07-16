54fd4a54be18ba09c1a2513ac6c5fa3209c92e1d
mikhael <mikhael@milikhin.name>
2017-07-16 23:03:25 +0700

    prepare first ChromeOS release

--- src/public/chrome/background.js
+++ src/public/chrome/background.js
@@ -5,8 +5,11 @@
  */
 
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('../index.html', {
-    id: 'main',
-    bounds: { width: 620, height: 500 }
-  });
+    chrome.app.window.create('../index.html', {
+        id: 'main',
+        bounds: {
+            width: 800,
+            height: 500
+        }
+    });
 });
